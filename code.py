8fcecf6cf0edddf69412a29234feae09d1313be6
unknown <ihabnoureldin9@gmail.com>
2020-08-15 21:26:27 +0300

    train-test-split

--- code.py
+++ code.py
@@ -4,4 +4,9 @@ import numpy as np
 spam_data = pd.read_csv('spam.csv')
 
 spam_data['target'] = np.where(spam_data['target']=='spam',1,0)
-spam_data.head(10)
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(spam_data['text'], 
+                                                    spam_data['target'], 
+                                                    random_state=0)
