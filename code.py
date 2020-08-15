64ea52831aecd03e00f9f764877e2b0fb4740db7
unknown <ihabnoureldin9@gmail.com>
2020-08-15 21:33:02 +0300

    question1

--- code.py
+++ code.py
@@ -10,3 +10,13 @@ from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(spam_data['text'], 
                                                     spam_data['target'], 
                                                     random_state=0)
+
+
+def answer_one():
+    
+    num_of_spam_entries = len(spam_data[spam_data['target'] == 1])
+    num_of_nospam_entries = len(spam_data) - num_of_spam_entries
+    spam_percentage = 100 * (num_of_spam_entries/num_of_nospam_entries) 
+    return spam_percentage
+
+print(answer_one())
